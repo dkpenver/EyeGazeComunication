1aa14456df0c588fe3af02b391750bea64900b43
dkpenver <pickled.mullet@gmail.com>
2017-05-05 11:15:19 +1200

    canny filter displayed

--- Main.cpp
+++ Main.cpp
@@ -8,12 +8,15 @@ int run = 1, step = 0;
 int main(int agrc, char * argc[]) {
 	video.open(1);
 	namedWindow(" Video ", WINDOW_AUTOSIZE);
-	Mat frame;
+	Mat frame, videoOut,gausOut;
+
 	while (true) {
 		if (run | step) {
 			video >> frame;
 			if (frame.empty()) break;
-			imshow(" Video ", frame);
+			GaussianBlur(frame, gausOut, Size(3, 3), 0, 0);
+			Canny(gausOut, videoOut, 20, 75);
+			imshow(" Video ", videoOut);
 		}
 		char c = waitKey(33);
 		if (c == 27) break;

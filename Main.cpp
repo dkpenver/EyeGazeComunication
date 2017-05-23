1771f5a664b6a5dbfce2dfaab809716fad5d4976
dkpenver <pickled.mullet@gmail.com>
2017-05-23 13:26:03 +1200

    Eye detction obtained
    i have completed taking the eye out of the image  but it is relitivly
    slow at the moment. I will need to find a way to optimise the system
    either by reducing the number of frames being processed or finding a
    work around.

--- Main.cpp
+++ Main.cpp
@@ -1,67 +1,99 @@
-#include<opencv2\opencv.hpp>
-#include "SkinDetector.h"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+
 #include <iostream>
 #include <stdio.h>
+
 using namespace std;
 using namespace cv;
+Mat cameraFeed;
 
-int main()
+/** Function Headers */
+void detectAndDisplay(Mat frame);
+
+/** Global variables */
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+string window_name = "Capture - Face detection";
+RNG rng(12345);
+
+/** @function main */
+int main(int argc, const char** argv)
 {
-	Mat src; Mat src_gray;
-	int thresh = 120;
-	int max_thresh = 255;
-	RNG rng(12345);
 	VideoCapture capture;
-	//open capture object at location zero (default location for webcam)
-
 	capture.open(1);
+	Mat frame;
 
-	//set height and width of capture frame
-	capture.set(CV_CAP_PROP_FRAME_WIDTH, 1080);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 1920);
+	//-- 1. Load the cascades
+	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading\n"); return -1; };
+	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading\n"); return -1; };
 
-	Mat cameraFeed,cannyOut;
-
-	SkinDetector mySkinDetector;
-
-	Mat skinMat,hist;
-
-	//start an infinite loop where webcam feed is copied to cameraFeed matrix
-	//all of our operations will be performed within this loop
-	while (1) {
-
-		//store image to matrix
-		capture.read(cameraFeed);
-
-		//show the current image
-		//imshow("Original Image", cameraFeed);
-
-		skinMat = mySkinDetector.getSkin(cameraFeed);
-
-		imshow("Skin Image", skinMat);
-		cvtColor(cameraFeed, cameraFeed, CV_BGR2GRAY);
-		////////////////////////////////////////////////////////////////////////////////////////////
-		Mat canny_output;
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-
-		/// Detect edges using canny
-		Canny(cameraFeed, canny_output, thresh, thresh * 2, 3);
-		/// Find contours
-		findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-		/// Draw contours
-		Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-		for (int i = 0; i< contours.size(); i++)
-		{
-			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-		}
-		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		imshow("Original Image", drawing);
-		if (waitKey(30) == 32) { break; };
+	//-- 2. Read the video stream
 	
-	}
+		while (true)
+		{
+			capture.read(frame);
+			//frame = cameraFeed;
+
+			//-- 3. Apply the classifier to the frame
+			if (!frame.empty())
+			{
+				detectAndDisplay(frame);
+			}
+			else
+			{
+				printf(" --(!) No captured frame -- Break!"); break;
+			}
+
+			int c = waitKey(1);
+			if ((char)c == 'c') { break; }
+		}
+	
 	return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay(Mat frame)
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+
+	cvtColor(frame, frame_gray, CV_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+		//ellipse(frame, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
+
+		//-- In each face, detect eyes
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			
+			Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			//circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
+			Mat eyeROI = faceROI(eyes[i]);
+			imshow(window_name, eyeROI);
+			//cout << eyes[j] << endl;
+		}
+	}
+	
+	/*int ymin = center[0] - raduis
+	Mat dst = frame(Range(center[0]-radius[0], ymax), Range(xMin, xMax)).clone();
+
+	Range::all() for all of x or y if needed
+	//-- Show what you got*/
+	
 }
